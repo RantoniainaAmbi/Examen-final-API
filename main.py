f65c06979c6f6ea61186c0db1dfa46c026c627f0
Ranto <hei.ranto.3@gmail.com>
2025-09-03 11:59:02 +0300

    feat: add specifications

--- main.py
+++ main.py
@@ -1,11 +1,28 @@
 import json
+from http.client import responses
 from typing import List
 from fastapi import FastAPI, Request
 from pydantic import BaseModel
-from starlette.responses import Response
+from starlette.responses import Response, JSONResponse
 
 app = FastAPI()
 
 @app.get("/ping")
 def ping():
     return {"ping": "pong"}
+
+
+@app.post("/cars")
+def create_car():
+    return
+
+@app.get("/cars")
+def read_cars(cars= cars):
+    return JSONResponse(content="This is the list of cars"+cars, status_code=200)
+
+@app.get("/cars/{id}")
+def find_cars(id : str):
+    if cars.id == id :
+        return JSONResponse(content=find_car, status_code=200)
+    else
+        return JSONResponse(content= "Not found", status_code=404)
