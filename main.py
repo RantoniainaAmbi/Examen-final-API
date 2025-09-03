4849e8ee1bf784dfc0893aeda2f16f87f4b0f8d2
Ranto <hei.ranto.3@gmail.com>
2025-09-03 12:14:25 +0300

    feat : add endpoints in main.py

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import json
+from asyncio import new_event_loop
 from http.client import responses
 from typing import List
 from fastapi import FastAPI, Request
 from pydantic import BaseModel
+from pyexpat.errors import messages
 from starlette.responses import Response, JSONResponse
 
 app = FastAPI()
@@ -11,18 +13,26 @@ app = FastAPI()
 def ping():
     return {"ping": "pong"}
 
+class cars(BaseModel):
+    id: str
+    brand: str
+    model : str
+    characteristics : object
+
+cars : List[cars]
 
 @app.post("/cars")
 def create_car():
-    return
+    cars.extend(cars)
+    return JSONResponse({messages: "Created"}, status_code=201)
 
 @app.get("/cars")
-def read_cars(cars= cars):
+def read_cars(cars : cars):
     return JSONResponse(content="This is the list of cars"+cars, status_code=200)
 
 @app.get("/cars/{id}")
 def find_cars(id : str):
     if cars.id == id :
-        return JSONResponse(content=find_car, status_code=200)
-    else
-        return JSONResponse(content= "Not found", status_code=404)
+        return JSONResponse({messages: "Ok"}, status_code=200)
+    else :
+        return JSONResponse({messages: "Not found"}, status_code=404)
